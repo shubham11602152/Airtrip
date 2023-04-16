c56bd6dcdee76c01c861d9e5b9c6e38d45807a1c
shubham11602152 <shubhammehta.333@gmail.com>
2023-04-16 23:32:35 +0530

    [Feat] Developed footer

--- src/common/components/Layout.jsx
+++ src/common/components/Layout.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main style={{ marginTop: "16px", minHeight: "calc(100vh - 72px)" }}>
+        {children}
+      </main>
+      <Footer />
     </>
   );
 }
