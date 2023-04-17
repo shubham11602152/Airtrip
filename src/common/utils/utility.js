18d3dea4e2984c0ed630741cf93f0d629a4a6fe3
shubham11602152 <shubhammehta.333@gmail.com>
2023-04-17 14:20:37 +0530

    [Feat] Created Seconds to HHMM conversion utility function

--- src/common/utils/utility.js
+++ src/common/utils/utility.js
@@ -1,29 +1,21 @@
-export const toReadableProperties = (data) => {
-  const {
-    fr: departureCity,
-    dt: departureTime,
-    to: arrivalCity,
-    at: arrivalTime,
-    al: airline,
-    ft: flightTime,
-    farepr: price,
-  } = data;
-
-  return {
-    departureCity,
-    departureTime,
-    arrivalCity,
-    arrivalTime,
-    airline,
-    flightTime,
-    price,
-  };
-};
-
 export const toCurrency_INR = (value) => {
   return value.toLocaleString("en-IN", {
     style: "currency",
     currency: "INR",
     maximumSignificantDigits: 3,
   });
+};
+
+export function padTo2Digits(num) {
+  return num.toString().padStart(2, "0");
+}
+
+export const secToHHMM = (seconds) => {
+  const milliSeconds = seconds * 1000;
+  const time = new Date(milliSeconds);
+
+  const HH = time.getUTCHours();
+  const MM = time.getUTCMinutes();
+
+  return `${HH}h ${MM}m`;
 };
